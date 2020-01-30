e64eea89a1c7089a2590b46ef3676b302e6efd56
Steeve Vadakkumchery <steeve.vadakkumchery@mmtdigital.co.uk>
2020-01-30 22:19:08 +0000

    added functionality to display alternative text if no list is available

--- src/components/TodoList/TodoList.test.tsx
+++ src/components/TodoList/TodoList.test.tsx
@@ -7,8 +7,32 @@ describe('<TodoList />', () => {
         const wrapper = shallow(<TodoList />);
     });
 
-    test('should render a unordered list', () => {
-        const wrapper = shallow(<TodoList />);
-        expect(wrapper.find('ul').length).toBe(1);
+    describe('if list is empty', () => {
+        test('should render message', () => {
+            const wrapper = shallow(<TodoList />);
+            expect(wrapper.find('span').text()).toBe('Nothing to do.');
+        });
+
+        test('should render message', () => {
+            const list = [];
+
+            const wrapper = shallow(<TodoList list={list} />);
+
+            expect(wrapper.find('span').text()).toBe('Nothing to do.');
+        });
     });
-})
+
+    describe('if list has one or more items', () => {
+        test('should show a simple list using li based on props', () => {
+            let list = [
+                {id: 1, text: 'washing', completed: false},
+                {id: 2, text: 'cooking', completed: false},
+                {id: 3, text: 'running', completed: false}
+            ];
+    
+            const wrapper = shallow(<TodoList list={list} />);
+
+            expect(wrapper.find('li').length).toBe(list.length);
+        });
+    });
+});
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const TodoList = () => {
-    return <ul></ul>;
+const TodoList = (props: { list?: {id: number, text: string, completed: boolean}[] }) => {
+    const { list } = props;
+    
+    if(list && list.length){
+        return <ul>{list && list.map((listItem) => <li key={listItem.id}>{listItem}</li>)}</ul>; 
+    } else {
+        return <span>Nothing to do.</span>; 
+    }
 }
 
 export default TodoList;
